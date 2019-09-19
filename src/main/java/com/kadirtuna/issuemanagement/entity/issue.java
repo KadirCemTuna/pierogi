e5ee87e27a2f841620f7227349e7fe149195461c
kadir.tuna <kadircemtuna@gmail.com>
2019-09-19 13:59:26 +0300

    First

--- src/main/java/com/kadirtuna/issuemanagement/entity/issue.java
+++ src/main/java/com/kadirtuna/issuemanagement/entity/issue.java
@@ -1,11 +1,26 @@
 package com.kadirtuna.issuemanagement.entity;
 
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
 import java.util.Date;
 
+
+@Entity
+@Table(name = "issue")
 public class issue extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @Column(name = "description",length = 400)
   private String description;
+
+  @Column(name = "date")
   private Date date;
+
+  @Enumerated(EnumType.STRING)
   private IssueStatus issueStatus;
 
 }
