8bcea6553564014711b98adb261f70a95482d43e
kadir.tuna <kadircemtuna@gmail.com>
2019-09-19 13:19:06 +0300

    First

---
+++ src/main/java/com/kadirtuna/issuemanagement/IssueManagementApplication.java
@@ -0,0 +1,16 @@
+package com.kadirtuna.issuemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagementApplication.class, args);
+
+
+
+	}
+
+}
